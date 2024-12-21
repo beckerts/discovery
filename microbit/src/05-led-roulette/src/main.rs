19324ca9c35acdbc043569a5ea71b92d0f96dd74
Steve Beckert <stevebeckert@hotmail.com>
2024-12-20 22:51:13 -0800

    Discovery 05 It blinks timer.delay

--- microbit/src/05-led-roulette/src/main.rs
+++ microbit/src/05-led-roulette/src/main.rs
@@ -3,15 +3,24 @@
 #![no_std]
 
 use cortex_m_rt::entry;
-use panic_halt as _;
-use microbit as _;
+use rtt_target::{rtt_init_print, rprintln};
+use panic_rtt_target as _;
+use microbit::board::Board;
+use microbit::hal::timer::Timer;
+use microbit::hal::prelude::*;
 
 #[entry]
 fn main() -> ! {
-    let _y;
-    let x = 42;
-    _y = x;
+    rtt_init_print!();
+    let mut board = Board::take().unwrap();
+    let mut timer = Timer::new(board.TIMER0);
+
+    board.display_pins.col1.set_low().unwrap();
+    board.display_pins.row1.set_high().unwrap();
 
     // infinite loop; just so we don't leave this stack frame
-    loop {}
+    loop {
+        timer.delay_ms(1000u16);
+        rprintln!("1000 ms passed");
+    }
 }
