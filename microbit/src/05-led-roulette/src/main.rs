38cebda5fdb30528f1c408f84c192c78bfe35f8b
Steve Beckert <stevebeckert@hotmail.com>
2024-12-26 22:53:34 -0800

    LED roulette  example solution

--- microbit/src/05-led-roulette/src/main.rs
+++ microbit/src/05-led-roulette/src/main.rs
@@ -11,6 +11,11 @@ use microbit::{
     hal::Timer,
 };
 
+const PIXELS: [(usize,usize); 16] = [
+    (0,0), (0,1), (0,2), (0,3), (0,4), (1,4), (2,4), (3,4), (4,4),
+    (4,3), (4,2), (4,1), (4,0), (3,0), (2,0), (1,0)
+];
+
 #[entry]
 fn main() -> ! {
     rtt_init_print!();
@@ -18,50 +23,16 @@ fn main() -> ! {
     let board = Board::take().unwrap();
     let mut timer = Timer::new(board.TIMER0);
     let mut display = Display::new(board.display_pins);
-    let mut lightarray = [[0; 5]; 5];
+    let mut leds = [[0; 5]; 5];
     let dur = 50;
+    let mut last_led = (0,0);
     
     loop {
-        // Show light_it_all for 1000ms
-        // display.show(&mut timer, light_it_all, 1000);
-        // // clear the display again
-        // display.clear();
-        // timer.delay_ms(1000_u32);
-        
-
-        for col in 0..5 {
-            if col > 0 {
-                lightarray[0][col - 1] = 0;
-            }
-            lightarray[0][col] = 1;
-            display.show(&mut timer, lightarray, dur);
-            //timer.delay_ms(500_u32);
+        for current_led in PIXELS.iter() {
+            leds[current_led.0][current_led.1] = 1;
+            leds[last_led.0][last_led.1] = 0;
+            display.show(&mut timer, leds, dur);
+            last_led = *current_led;
         }
-
-        for row in 1..5 {
-            if row > 0 {
-                lightarray[row - 1][4] = 0;
-            }
-            lightarray[row][4] = 1;
-            display.show(&mut timer, lightarray, dur);
-            //timer.delay_ms(500_u32);
-        }
-
-        for col in (0..4).rev() {
-            lightarray[4][col + 1] = 0;
-            lightarray[4][col] = 1;
-            display.show(&mut timer, lightarray, dur);
-            //timer.delay_ms(500_u32);
-        }
-
-        for row in (1..4).rev() {
-            lightarray[row + 1][0] = 0;
-            lightarray[row][0] = 1;
-            display.show(&mut timer, lightarray, dur);
-            //timer.delay_ms(500_u32);
-        }
-
-        lightarray[1][0] = 0;
-        display.clear();
     }
 }
